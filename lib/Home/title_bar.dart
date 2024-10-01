48b780f664ae220bf115d33209acaaa6359cee38
Woisol-G <Woisol-G@outlook.com>
2024-10-01 13:04:51 +0800

    refactor: 组件化了TitleBar的按钮

--- lib/Home/title_bar.dart
+++ lib/Home/title_bar.dart
@@ -125,72 +125,57 @@ class _TitleBarState extends State<TitleBar> with WindowListener {
                 backgroundColor: Colors.grey[200],
                 actions: <Widget>[
           // Material(borderRadius: const BorderRadius.all(Radius.zero),child:
-          IconButton(
-            // !解决了radius后还是用回IconButton，因为另外的OutlineButton即使设置了padding依然过长，IconButton的样式就挺合适
-            iconSize: 20,
-            icon: const Icon(Icons.remove),
-            alignment: Alignment.center,
-            splashRadius: 1,
-            // !必须>0……
-            tooltip: '最小化',
-
-            // ~~尝试OutlinedButton，甚至无法使用style咳咳你下面用着了……
-            // child: Icon(Icons.remove, size: 20,color: Colors.black,),
-
-            // style: ButtonStyle(
-            //   splashFactory: NoSplash.splashFactory,
-            //   // shape: MaterialStateProperty.all(CircleBorder()),
-            //   // shape: Rect.fromCenter(center: 0, width: 0, height: 0)
-            // ),
-
-            // style: RoundedRectangleBorder(
-            //     borderRadius: BorderRadius.circular(0)
-            // ),
-            // color: WindowCaptionButton.close(),
-
-            style: OutlinedButton.styleFrom(
-                splashFactory: NoSplash.splashFactory,
-                padding: const EdgeInsets.all(5),
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero)),
-            onPressed: () {
-              windowManager.minimize();
-            },
-          ),
-          // ),
-          IconButton(
-            iconSize: 20,
-            icon: const Icon(Icons.square_outlined),
-            alignment: Alignment.center,
-            splashRadius: 1,
-            // !必须>0……
-            tooltip: '最大化',
-            style: OutlinedButton.styleFrom(
-                splashFactory: NoSplash.splashFactory,
-                padding: const EdgeInsets.all(5),
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero)),
-            onPressed: () {
-              windowManager.maximize();
-            },
-          ),
-          IconButton(
-            iconSize: 20,
-            icon: const Icon(Icons.close),
-            alignment: Alignment.center,
-            hoverColor: Colors.red[400],
-            splashRadius: 1,
-            // !必须>0……
-            tooltip: '关闭',
-            style: OutlinedButton.styleFrom(
-                splashFactory: NoSplash.splashFactory,
-                padding: const EdgeInsets.all(5),
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.zero)),
-            onPressed: () {
-              windowManager.close();
-            },
-          ),
+          TitleBarBtn(
+              icon: Icons.remove,
+              tooltip: '最小化',
+              onPressed: () {
+                windowManager.minimize();
+              }),
+          TitleBarBtn(
+              icon: Icons.square_outlined,
+              tooltip: '最大化',
+              onPressed: () {
+                windowManager.maximize();
+              }),
+          TitleBarBtn(
+              icon: Icons.close,
+              tooltip: '关闭',
+              hoverColor: Colors.red[400],
+              onPressed: () {
+                windowManager.close();
+              }),
         ]));
+  }
+}
+
+class TitleBarBtn extends StatelessWidget {
+  late IconData icon;
+  late String tooltip;
+  late VoidCallback? onPressed;
+  late Color hoverColor;
+  TitleBarBtn(
+      {required this.icon,
+      required this.tooltip,
+      required this.onPressed,
+      Color? hoverColor})
+      : hoverColor = hoverColor ?? Colors.grey[300]!;
+  // !md混乱……下面这个就是死都不行……
+  // this.hoverColor = const Colors.grey[200]!});
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      iconSize: 20,
+      icon: Icon(icon),
+      alignment: Alignment.center,
+      hoverColor: hoverColor,
+      splashRadius: 1,
+      // !必须>0……
+      tooltip: tooltip,
+      style: OutlinedButton.styleFrom(
+          splashFactory: NoSplash.splashFactory,
+          padding: const EdgeInsets.all(5),
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero)),
+      onPressed: onPressed,
+    );
   }
 }
