ae41e1e327ff2bd0085a6f4a2e12765f576cfc59
Woisol-G <Woisol-G@outlook.com>
2024-10-02 17:45:28 +0800

    feat: 完成部分计算界面的设计，预备重构组件树

--- lib/Expression/expression.dart
+++ lib/Expression/expression.dart
@@ -11,7 +11,34 @@ late List<String> expressionSample = [
   "8/(9-9)",
   "90-(5-4)*2-2^3",
 ].map((e) => expressionFormat(e)).toList();
+// List<Map<String, String>> expressionSample2 = [{},{}];
+// List<String[2]> expressionSample2 = [
+//   {"7", "7"},
+//   {"8", "8"},
+//   {"9", "9"},
+//   {}
+// ];
+List<List<String>> calKeyBoardButtonList = [
+  ["7", "7"],
+  ["8", "8"],
+  ["9", "9"],
+  [".", "."],
+  ["+", "+"],
+  ["-", "-"],
+  ["^", "^"],
+  ["4", "4"],
+  ["5", "5"],
+  ["6", "6"],
+  ["1", "1"],
+  ["2", "2"],
+  ["3", "3"],
+  ["0", "0"],
+  ["*", "*"],
+  ["/", "/"],
+  ["=", "="]
+];
 
+// !并不支持这样的定义……
 class PageExpression extends StatelessWidget {
   TextEditingController controller = TextEditingController();
   String result = "";
@@ -72,12 +99,70 @@ class PageExpression extends StatelessWidget {
               decoration: BoxDecoration(
                   // color: Colors.white,
                   borderRadius: BorderRadius.all(Radius.circular(10))),
-            )
+            ),
+            // Expanded(
+            //     child: Container(
+            //   width: double.infinity,
+            //   height: double.infinity,
+            //   child: GridView(
+            //       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            //           crossAxisCount: 7,
+            //           // childAspectRatio: 1.5,
+            //           crossAxisSpacing: 6,
+            //           mainAxisSpacing: 10),
+            //       children: <Widget>[
+            //         OutlinedButton(onPressed: () {}, child: const Text("7")),
+            //         OutlinedButton(onPressed: () {}, child: const Text("8")),
+            //         OutlinedButton(onPressed: () {}, child: const Text("9")),
+            //       ]
+            //       // calKeyBoardButtonList
+            //       //     .map((key) => calKeyBoardButton(key[0], key[1]))
+            //       //     .toList(),
+            //       // [
+            //       //   OutlinedButton(
+            //       //       onPressed: () {
+            //       //         controller.clear();
+            //       //         result = "";
+            //       //       },
+            //       //       child: Text("清空")),
+            //       // ],
+            //       ),
+            // ))
           ],
         ));
+  }
+
+  OutlinedButton calKeyBoardButton(String text, String? pressValue) {
+    // ;)组件化也不一定要class嘛
+    return OutlinedButton(
+      style: ButtonStyle(
+        // !？？？这里函数化组件必须用这个WidgetStatePropertyAll来提供参数？？
+
+        backgroundColor:
+            WidgetStatePropertyAll(text == "=" ? Colors.yellow : Colors.white),
+        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        )),
+      ),
+      child: Text(text),
+      onPressed: () {
+        controller.text += pressValue ?? text;
+      },
+    );
   }
 }
 // td Incorrect use of ParentDataWidget.，另一个Multinomial没问题看看这里……不对另一个跳转多了也会崩……
 
 // class _PageExpressionState extends State<PageExpression> {
+// }
+// !az不能嵌套class……
+// class CalKeyBoardButton extends StatelessWidget {
+//   late String text;
+//   late String? pressValue;
+//   // late Function()? onButtonPressed;
+// // late type
+// CalKeyBoardButton({required this.text, this.pressValue});
+// @override
+// Widget build(BuildContext context) {
+//   return ElevatedButton(onPressed: , child: child)
 // }
